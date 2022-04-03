a99ba5210e6fadfe6982cf9c2b67d1d5ad875fe7
mac_hoon <mac_hoon@218.38.137.28>
2022-04-04 08:50:06 +0900

    4/4 psh 8:50

--- FoodContainer/src/main/java/team/project/dao/MemberDAO.java
+++ FoodContainer/src/main/java/team/project/dao/MemberDAO.java
@@ -3,6 +3,7 @@ package team.project.dao;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -19,4 +20,11 @@ public class MemberDAO {
 		
 		return sqlSession.selectList(Namespace+".selectMember",vo);
 	}
+	
+	 @Autowired
+	    private SqlSessionTemplate mybatis;
+
+	    public int Login(MemberVO vo) throws Exception {
+	        return mybatis.selectOne("Member.Login", vo);
+	    }
 }
