e725aca8c24f0dcc8f7512aa7ab666a95ac3fa3a
Bono-Shin <ehdghks133@naver.com>
2022-04-15 01:22:32 +0900

    sdh

--- FoodContainer/src/main/java/team/project/controller/ShopBasketController.java
+++ FoodContainer/src/main/java/team/project/controller/ShopBasketController.java
@@ -1,6 +1,7 @@
 package team.project.controller;
 
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,6 +10,7 @@ import java.util.Locale;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,12 +38,14 @@ public class ShopBasketController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "shopBasket_main.do", method = RequestMethod.GET)
-	public String home(Locale locale, Model model, HttpServletRequest request, ProductVO productVO) throws Exception{
+	public String home(Locale locale, Model model, HttpServletRequest request, ProductVO productVO, HttpServletResponse response) throws Exception{
 		
 		Cookie[] cookies = request.getCookies();
 		String noMemberCartCookie = null;
+		String product_index = request.getParameter("product_index");
+		String cnt = request.getParameter("cnt");
+		String changeCookie = null;
 		ArrayList<String> noMemberCartArr = new ArrayList<>();
-		ArrayList<String> noMemberCartArrProductCnt = new ArrayList<>();
 		
 		if(cookies != null) {
 			for(Cookie cookie : cookies) {
@@ -53,10 +57,41 @@ public class ShopBasketController {
 		
 		if(noMemberCartCookie != null) {
 			String[] tempCookieArr = noMemberCartCookie.split(",");
+			if(product_index != null && cnt != null) {
+				
+				for(int i=0; i<tempCookieArr.length; i++) {
+					
+					if(tempCookieArr[i].substring(0,tempCookieArr[i].length()-1).equals(product_index)) {
+						tempCookieArr[i] = product_index + cnt;
+						
+						if(i == 0) {
+							changeCookie = tempCookieArr[i];
+							System.out.println("1 ="+changeCookie);
+						}else {
+							String tempCookie = URLDecoder.decode(changeCookie,"UTF-8");
+							changeCookie = URLEncoder.encode(tempCookie.toString()+","+tempCookieArr[i],"UTF-8");
+							System.out.println("2 ="+changeCookie);
+						}
+						
+					}else {
+						if(i == 0) {
+							changeCookie = tempCookieArr[i];
+							System.out.println("3 ="+changeCookie);
+						}else {
+							String tempCookie = URLDecoder.decode(changeCookie,"UTF-8");
+							changeCookie = URLEncoder.encode(tempCookie.toString()+","+tempCookieArr[i],"UTF-8");
+							System.out.println("4 ="+changeCookie);
+						}
+					}
+				}
+				
+				Cookie newCookie = new Cookie("noMemberCart",changeCookie);
+				
+				response.addCookie(newCookie);
+			}
 			
 			for(int i=0; i<tempCookieArr.length; i++) {
 				noMemberCartArr.add(tempCookieArr[i].substring(0,tempCookieArr[i].length()-1));
-				noMemberCartArrProductCnt.add(tempCookieArr[i].substring(tempCookieArr[i].length()-1,tempCookieArr[i].length()));
 			}
 			
 			List<ProductVO> noMemberCartList = productService.noMemberCartList(noMemberCartArr);
