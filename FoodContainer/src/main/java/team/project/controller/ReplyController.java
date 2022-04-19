c99f3e9a1dcc4784d653ff25f785a6614251a8f5
mac_hoon <mac_hoon@localhost>
2022-04-19 17:45:14 +0900

    4/19 psh2

--- FoodContainer/src/main/java/team/project/controller/ReplyController.java
+++ FoodContainer/src/main/java/team/project/controller/ReplyController.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import team.project.service.ReplyService;
 import team.project.vo.MemberVO;
-import team.project.vo.RecipeVO;
 import team.project.vo.ReplyVO;
 
 @Controller
@@ -22,7 +22,7 @@ public class ReplyController {
 	
 	
 	
-	
+	//댓글 작성
 	@RequestMapping(value = "/wirteReply",method = RequestMethod.POST)
 	public String wirteReply(ReplyVO vo,RedirectAttributes rttr,HttpServletRequest request) throws Exception{
 		
@@ -30,18 +30,39 @@ public class ReplyController {
 		//회원
 		MemberVO member = (MemberVO)session.getAttribute("member");
 		vo.setMember_index(member.getMember_index());
-		
-		
-		
-		System.out.println("==============================");
-		System.out.println(vo.getRecipe_index());
-		
-//		rttr.addAttribute("recipe_index",vo.getRecipe_index());
-//		rttr.addAttribute("member_index",vo.getMember_index());
-//		rttr.addAttribute("contents", vo.getContents());
 		replyService.writeReply(vo);
 		return "redirect:recipeview.do?recipe_index="+vo.getRecipe_index();
 	}
+	
+	//댓글 수정 GET
+//		@RequestMapping(value="/recipeview_reply_modify.do", method = RequestMethod.GET)
+//		public String replyUpdateView(ReplyVO vo, Model model) throws Exception {
+//			
+//			
+//			model.addAttribute("updateReply",vo);
+//			
+//			
+//			return "board/updateReply";
+//		}
+		
+		//댓글 수정 POST
+		@RequestMapping(value="/updateReply", method = RequestMethod.POST)
+		public String updateReply(ReplyVO vo, HttpServletRequest request) throws Exception {
+			
+			HttpSession session = request.getSession();
+			MemberVO member = (MemberVO)session.getAttribute("member");
+			
+			vo.setReply_index(vo.getReply_index());
+			vo.setRecipe_index(vo.getRecipe_index());
+			vo.setMember_index(member.getMember_index());
+			vo.setContents(vo.getContents());
+			replyService.updateReply(vo);
+			
+			
+			
+			return "redirect:/board/readView";
+		}
+	
 	
 	
 }
