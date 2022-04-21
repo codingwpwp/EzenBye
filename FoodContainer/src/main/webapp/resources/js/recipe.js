e903ac510f46e1b9c4bca85ae0377c9c1ac8e695
mac_hoon <mac_hoon@localhost>
2022-04-21 17:21:43 +0900

    psh 4/21

--- FoodContainer/src/main/webapp/resources/js/recipe.js
+++ FoodContainer/src/main/webapp/resources/js/recipe.js
@@ -6,11 +6,7 @@
             toolbar: [
                 ['style', ['style']],
                 ['font', ['bold', 'underline', 'clear']],
-                ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['table', ['table']],
-                ['insert', ['link', 'picture', 'video']],
-                ['view', ['fullscreen', 'codeview', 'help']]
+                
             ]
         });
         function best(){
